eee753953766188e01dc829832424bb87857532d
Rul/E <136341720+syhrlf-e@users.noreply.github.com>
2026-06-26 17:12:39 +0700

    feat(semantic): validate scopes and program semantics

---
+++ src/semantic/semantic-analyzer.js
@@ -0,0 +1,314 @@
+'use strict'
+
+const { CompilerError } = require('../errors/compiler-error')
+const { getErrorMessage } = require('../errors/messages')
+
+const ValueType = Object.freeze({
+  NUMBER: 'number',
+  STRING: 'string',
+  BOOLEAN: 'boolean',
+  NULL: 'null',
+  UNDEFINED: 'undefined',
+  UNKNOWN: 'unknown',
+  FUNCTION: 'function'
+})
+
+class ScopeChain {
+  constructor() {
+    this.scopes = [new Map()]
+  }
+
+  enter() {
+    this.scopes.push(new Map())
+  }
+
+  exit() {
+    if (this.scopes.length === 1) throw new Error('Global scope tidak boleh ditutup.')
+    this.scopes.pop()
+  }
+
+  define(name, symbol) {
+    const currentScope = this.scopes.at(-1)
+    if (currentScope.has(name)) return false
+    currentScope.set(name, symbol)
+    return true
+  }
+
+  lookup(name) {
+    for (let index = this.scopes.length - 1; index >= 0; index -= 1) {
+      const symbol = this.scopes[index].get(name)
+      if (symbol) return symbol
+    }
+    return null
+  }
+}
+
+class SemanticAnalyzer {
+  constructor(source = null) {
+    this.source = source
+    this.scope = new ScopeChain()
+    this.functionDepth = 0
+    this.loopDepth = 0
+  }
+
+  analyze(ast) {
+    this.visit(ast)
+    return ast
+  }
+
+  visit(node) {
+    switch (node.type) {
+      case 'Program':
+        for (const statement of node.body) this.visit(statement)
+        return ValueType.UNKNOWN
+      case 'Block': return this.visitBlock(node)
+      case 'VarDecl':
+      case 'ConstDecl': return this.visitDeclaration(node)
+      case 'AssignStmt': return this.visitAssignment(node)
+      case 'UpdateStmt': return this.visitUpdate(node)
+      case 'PrintStmt': return this.visit(node.value)
+      case 'ExprStmt': return this.visit(node.expression)
+      case 'IfStmt': return this.visitIfStatement(node)
+      case 'WhileStmt': return this.visitWhileStatement(node)
+      case 'ForStmt': return this.visitForStatement(node)
+      case 'FuncDecl': return this.visitFunctionDeclaration(node)
+      case 'ReturnStmt':
+        if (this.functionDepth === 0) this.raise('E08', node, node.loc.line)
+        return node.value ? this.visit(node.value) : ValueType.UNDEFINED
+      case 'BreakStmt':
+        if (this.loopDepth === 0) this.raise('E09', node, node.loc.line)
+        return ValueType.UNKNOWN
+      case 'ContinueStmt':
+        if (this.loopDepth === 0) this.raise('E10', node, node.loc.line)
+        return ValueType.UNKNOWN
+      case 'BinaryExpr': return this.visitBinaryExpression(node)
+      case 'UnaryExpr': return this.visitUnaryExpression(node)
+      case 'CallExpr': return this.visitCallExpression(node)
+      case 'Identifier': return this.lookupVariable(node).type
+      case 'NumberLiteral': return ValueType.NUMBER
+      case 'StringLiteral': return ValueType.STRING
+      case 'BooleanLiteral': return ValueType.BOOLEAN
+      case 'NullLiteral': return ValueType.NULL
+      case 'UndefinedLiteral': return ValueType.UNDEFINED
+      default: throw new Error(`AST node tidak didukung: ${node.type}`)
+    }
+  }
+
+  visitBlock(node) {
+    this.scope.enter()
+    try {
+      for (const statement of node.body) this.visit(statement)
+    } finally {
+      this.scope.exit()
+    }
+    return ValueType.UNKNOWN
+  }
+
+  visitDeclaration(node) {
+    const valueType = this.visit(node.value)
+    const symbol = { kind: 'variable', mutable: node.mutable, type: valueType, line: node.loc.line }
+    if (!this.scope.define(node.name, symbol)) this.raise('E11', node, node.name)
+    return valueType
+  }
+
+  visitAssignment(node) {
+    const symbol = this.lookupVariable(node)
+    if (!symbol.mutable) this.raise('E06', node, node.loc.line, node.name)
+    const valueType = this.visit(node.value)
+
+    if (node.operator === '=') {
+      this.assertAssignable(symbol, valueType, node)
+      this.updateInferredType(symbol, valueType)
+      return symbol.type
+    }
+
+    if (node.operator === '+=') {
+      const resultType = this.resolveAdditionType(symbol.type, valueType, node)
+      this.updateInferredType(symbol, resultType)
+      return resultType
+    }
+
+    this.assertNumber(symbol.type, node)
+    this.assertNumber(valueType, node)
+    if (node.operator === '/=' && this.isZeroLiteral(node.value)) this.raise('E07', node, node.loc.line)
+    return ValueType.NUMBER
+  }
+
+  visitUpdate(node) {
+    const symbol = this.lookupVariable(node)
+    if (!symbol.mutable) this.raise('E06', node, node.loc.line, node.name)
+    this.assertNumber(symbol.type, node)
+    this.updateInferredType(symbol, ValueType.NUMBER)
+    return ValueType.NUMBER
+  }
+
+  visitIfStatement(node) {
+    this.assertBoolean(this.visit(node.condition), node.condition)
+    this.visit(node.consequent)
+    for (const alternate of node.alternates) {
+      this.assertBoolean(this.visit(alternate.condition), alternate.condition)
+      this.visit(alternate.block)
+    }
+    if (node.fallback) this.visit(node.fallback)
+    return ValueType.UNKNOWN
+  }
+
+  visitWhileStatement(node) {
+    this.assertBoolean(this.visit(node.condition), node.condition)
+    this.loopDepth += 1
+    try { this.visit(node.body) } finally { this.loopDepth -= 1 }
+    return ValueType.UNKNOWN
+  }
+
+  visitForStatement(node) {
+    this.scope.enter()
+    try {
+      const initializerType = this.visit(node.init.value)
+      if (!this.scope.define(node.init.name, {
+        kind: 'variable', mutable: true, type: initializerType, line: node.init.loc.line
+      })) this.raise('E11', node.init, node.init.name)
+
+      this.assertBoolean(this.visit(node.condition), node.condition)
+      this.loopDepth += 1
+      try {
+        this.visit(node.update)
+        this.visit(node.body)
+      } finally {
+        this.loopDepth -= 1
+      }
+    } finally {
+      this.scope.exit()
+    }
+    return ValueType.UNKNOWN
+  }
+
+  visitFunctionDeclaration(node) {
+    if (!this.scope.define(node.name, {
+      kind: 'function', mutable: false, type: ValueType.FUNCTION, params: node.params, line: node.loc.line
+    })) this.raise('E11', node, node.name)
+
+    const outerLoopDepth = this.loopDepth
+    this.scope.enter()
+    this.functionDepth += 1
+    this.loopDepth = 0
+    try {
+      for (const parameter of node.params) {
+        if (!this.scope.define(parameter, {
+          kind: 'variable', mutable: true, type: ValueType.UNKNOWN, line: node.loc.line
+        })) this.raise('E11', node, parameter)
+      }
+      this.visit(node.body)
+    } finally {
+      this.loopDepth = outerLoopDepth
+      this.functionDepth -= 1
+      this.scope.exit()
+    }
+    return ValueType.FUNCTION
+  }
+
+  visitBinaryExpression(node) {
+    const leftType = this.visit(node.left)
+    const rightType = this.visit(node.right)
+    if (node.operator === '+') return this.resolveAdditionType(leftType, rightType, node)
+
+    if (['-', '*', '/', '%'].includes(node.operator)) {
+      this.assertNumber(leftType, node.left)
+      this.assertNumber(rightType, node.right)
+      if (node.operator === '/' && this.isZeroLiteral(node.right)) this.raise('E07', node, node.loc.line)
+      return ValueType.NUMBER
+    }
+    if (['>', '<', '>=', '<='].includes(node.operator)) {
+      this.assertNumber(leftType, node.left)
+      this.assertNumber(rightType, node.right)
+      return ValueType.BOOLEAN
+    }
+    if (['==', '!='].includes(node.operator)) {
+      this.assertComparable(leftType, rightType, node)
+      return ValueType.BOOLEAN
+    }
+    if (['jo', 'atau'].includes(node.operator)) {
+      this.assertBoolean(leftType, node.left)
+      this.assertBoolean(rightType, node.right)
+      return ValueType.BOOLEAN
+    }
+    throw new Error(`Operator tidak didukung: ${node.operator}`)
+  }
+
+  visitUnaryExpression(node) {
+    this.assertBoolean(this.visit(node.value), node.value)
+    return ValueType.BOOLEAN
+  }
+
+  visitCallExpression(node) {
+    const symbol = this.scope.lookup(node.name)
+    if (!symbol || symbol.kind !== 'function') this.raise('E04', node, node.name)
+    if (node.args.length !== symbol.params.length) this.raise('E03', node, node.loc.line)
+    for (const argument of node.args) this.visit(argument)
+    return ValueType.UNKNOWN
+  }
+
+  lookupVariable(node) {
+    const symbol = this.scope.lookup(node.name)
+    if (!symbol || symbol.kind !== 'variable') this.raise('E02', node, node.name)
+    return symbol
+  }
+
+  resolveAdditionType(leftType, rightType, node) {
+    if (leftType === ValueType.STRING || rightType === ValueType.STRING) {
+      if (this.isKnown(leftType) && this.isKnown(rightType)) return ValueType.STRING
+      return ValueType.UNKNOWN
+    }
+    this.assertNumber(leftType, node)
+    this.assertNumber(rightType, node)
+    return ValueType.NUMBER
+  }
+
+  assertAssignable(symbol, valueType, node) {
+    if (!this.isKnown(symbol.type) || !this.isKnown(valueType)) return
+    if ([ValueType.NULL, ValueType.UNDEFINED].includes(symbol.type)) return
+    if (symbol.type !== valueType) this.raise('E03', node, node.loc.line)
+  }
+
+  assertComparable(leftType, rightType, node) {
+    if (!this.isKnown(leftType) || !this.isKnown(rightType)) return
+    if ([ValueType.NULL, ValueType.UNDEFINED].includes(leftType)) return
+    if ([ValueType.NULL, ValueType.UNDEFINED].includes(rightType)) return
+    if (leftType !== rightType) this.raise('E03', node, node.loc.line)
+  }
+
+  assertNumber(type, node) {
+    if (type !== ValueType.NUMBER && type !== ValueType.UNKNOWN) this.raise('E03', node, node.loc.line)
+  }
+
+  assertBoolean(type, node) {
+    if (type !== ValueType.BOOLEAN && type !== ValueType.UNKNOWN) this.raise('E03', node, node.loc.line)
+  }
+
+  updateInferredType(symbol, type) {
+    if ([ValueType.NULL, ValueType.UNDEFINED, ValueType.UNKNOWN].includes(symbol.type)) symbol.type = type
+  }
+
+  isKnown(type) {
+    return type !== ValueType.UNKNOWN
+  }
+
+  isZeroLiteral(node) {
+    return node.type === 'NumberLiteral' && node.value === 0
+  }
+
+  raise(code, node, ...messageArguments) {
+    throw new CompilerError({
+      code,
+      message: getErrorMessage(code, ...messageArguments),
+      line: node.loc?.line ?? null,
+      column: node.loc?.column ?? null,
+      source: this.source
+    })
+  }
+}
+
+function analyze(ast, source) {
+  return new SemanticAnalyzer(source).analyze(ast)
+}
+
+module.exports = { analyze, ScopeChain, SemanticAnalyzer, ValueType }
---
+++ tests/semantic-analyzer.test.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const test = require('node:test')
+const assert = require('node:assert/strict')
+const { CompilerError } = require('../src/errors/compiler-error')
+const { tokenize } = require('../src/lexer/lexer')
+const { parse } = require('../src/parser/parser')
+const { analyze } = require('../src/semantic/semantic-analyzer')
+
+function analyzeSource(source) {
+  return analyze(parse(tokenize(source), source), source)
+}
+
+function expectError(source, code) {
+  assert.throws(
+    () => analyzeSource(source),
+    (error) => error instanceof CompilerError && error.code === code
+  )
+}
+
+test('semantic analyzer menerima program valid dengan scope block dan fungsi rekursif', () => {
+  const source = [
+    'karajo faktorial(n) {',
+    '  jiko (n <= 1) { baliakan 1 }',
+    '  baliakan n * faktorial(n - 1)',
+    '}',
+    'buek hasil = faktorial(5)',
+    'cetak hasil'
+  ].join('\n')
+
+  assert.equal(analyzeSource(source).type, 'Program')
+})
+
+test('semantic analyzer memperbolehkan shadowing namun menolak deklarasi ganda satu scope', () => {
+  assert.equal(analyzeSource('buek nilai = 1 jiko (batua) { buek nilai = 2 cetak nilai }').type, 'Program')
+  expectError('buek nilai = 1 buek nilai = 2', 'E11')
+})
+
+test('semantic analyzer menolak variabel tidak terdeklarasi dan perubahan konstanta', () => {
+  expectError('cetak namo', 'E02')
+  expectError('tapek pi = 3.14 pi = 3', 'E06')
+})
+
+test('semantic analyzer memvalidasi pemanggilan fungsi dan jumlah argumennya', () => {
+  expectError('cetak tidak_ado()', 'E04')
+  expectError('karajo tambah(a, b) { baliakan a + b } cetak tambah(1)', 'E03')
+})
+
+test('semantic analyzer memvalidasi konteks return, break, dan continue', () => {
+  expectError('baliakan 1', 'E08')
+  expectError('baranti', 'E09')
+  expectError('lanjuik', 'E10')
+  assert.equal(analyzeSource('salamo (batua) { lanjuik baranti }').type, 'Program')
+})
+
+test('semantic analyzer membuat initializer untuak sebagai variabel lokal loop', () => {
+  assert.equal(analyzeSource('untuak (i = 0; i < 3; i++) { cetak i }').type, 'Program')
+  expectError('untuak (i = 0; i < 3; i++) { cetak i } cetak i', 'E02')
+})
+
+test('semantic analyzer menangkap pembagian nol literal dan ketidaksesuaian tipe', () => {
+  expectError('buek hasil = 10 / 0', 'E07')
+  expectError("buek umur = 20 umur = 'dua puluh'", 'E03')
+  expectError('jiko (1) { cetak 1 }', 'E03')
+})
+
+test('semantic analyzer mendukung konkatenasi string dan assignment dari nilai undefined', () => {
+  assert.equal(analyzeSource("buek namo = 'Rull' cetak 'Halo, ' + namo").type, 'Program')
+  assert.equal(analyzeSource('buek hasil = datantu hasil = 10 cetak hasil').type, 'Program')
+})
